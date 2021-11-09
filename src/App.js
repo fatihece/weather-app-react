6c5c1b8853350c31c229c174aa3c55ce8c2f9982
fatih <fatihenaz@gmail.com>
2021-11-09 20:34:02 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import {useState} from "react"
 import './App.css';
+import Card from './components/Card';
 
 function App() {
+  const [city, setCity] = useState("")
+  console.log(city);
   return (
     <div className="App">
       <h1>Daily Forecast</h1>
+      <div className="input_container">
+        <input type="text" className="input_text" placeholder=" Search city..." value={city} onChange={(e)=> setCity(e.target.value)}/>
+        <button className="btn">Search</button>
+      </div>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react'
+import "./Card.css"
+
+const Card = () => {
+
+    useEffect(() => {
+        
+    }, [])
+
+    return (
+        <div className="card">
+            This is Card
+        </div>
+    )
+}
+
+export default Card
